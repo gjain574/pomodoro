63ae10d061b8b0e794ea8d4d93778178f35edf01
Gautam Jain <19173627+gjain574@users.noreply.github.com>
2021-10-01 21:39:52 +0530

    Add calculateTimeRemaining function

--- src/models/timers.js
+++ src/models/timers.js
@@ -2,17 +2,48 @@ const Moment = require('moment');
 const { v4: uuidv4 } = require('uuid');
 const { pgPoolClient } = require('../db/db_clients');
 
+const calculateTimeRemaining = (rows) => {
+	return rows.map((row) => {
+		const { created_at, duration, total_time_clocked, last_updated_at, status } = row;
+
+		let time_passed;
+		let time_remaining;
+
+		switch (status){
+			case 'running':
+				time_passed = Moment().unix() - Moment(created_at).unix();
+				time_remaining = duration - time_passed;
+				break;
+			case 'paused':
+				time_passed = total_time_clocked;
+				time_remaining = duration - time_passed;
+				break;
+			case 'resumed':
+				time_passed = Moment().unix() - Moment(last_updated_at).unix() + total_time_clocked;
+				time_remaining = duration - time_passed;
+				break;
+			case 'completed':
+				time_remaining = 0;
+				break;
+		}
+		row['time_remaining'] = time_remaining;
+		return row
+	})
+}
+
+module.exports.calculateTimeRemaining = calculateTimeRemaining;
 
 // fetch a single timer and its info
 const fetchTimer = async (id) => {
 	const { rows, rowCount } = await pgPoolClient.query(`SELECT * FROM timers WHERE id = '${id}';`);
-	return rows[0]
+	const updated_rows = calculateTimeRemaining(rows);
+	return updated_rows[0]
 }
 
 module.exports.fetchTimer = fetchTimer;
 
 // default duration is 25 mins ~ 1500 seconds
-const createTimer = async (name, duration = 1500) => {
+const createTimer = async (name, duration = 1500, webhook_url = '') => {
 	const id = uuidv4();
 	try {
 		const { rows } = await pgPoolClient.query(`INSERT INTO timers 
@@ -21,6 +52,7 @@ const createTimer = async (name, duration = 1500) => {
 				name,
 				duration,
 				total_time_clocked,
+				webhook_url,
 				status
 			)
 
@@ -30,6 +62,7 @@ const createTimer = async (name, duration = 1500) => {
 				'${name}',
 				${duration},
 				${0},
+				'${webhook_url}',
 				'running'
 			)
 
@@ -48,20 +81,42 @@ module.exports.createTimer = createTimer;
 // fetch list of all timers sorted by created_date
 const fetchTimers = async () => {
 	const { rows, rowCount } = await pgPoolClient.query(`SELECT * FROM timers ORDER BY created_at DESC LIMIT 10;`);
-	return { count: rowCount, timers: rows }
+	const updated_rows = calculateTimeRemaining(rows);
+	return { count: rowCount, timers: updated_rows }
 }
 
 module.exports.fetchTimers = fetchTimers;
 
 // The below function will be responsible for both resuming and pausing a timer.
 const updateTimer = async (id, status) => {
-	const { rows } = await pgPoolClient.query(`
-		UPDATE timers 
-		SET status = '${status}', last_updated_at = now()
-		WHERE id = '${id}'
-		RETURNING *;
-	`)
-	return rows[0]
+
+	const response_data = await pgPoolClient.query(`SELECT * FROM timers WHERE id = '${id}';`);
+
+	if (response_data.rowCount === 1){
+		const { last_updated_at, total_time_clocked } = response_data.rows[0];
+		const updated_total_time_clocked = Moment().unix() - Moment(last_updated_at).unix() + total_time_clocked;
+		let query;
+		if (status === "paused"){
+			query = `
+				UPDATE timers 
+				SET status = '${status}', last_updated_at = now(), total_time_clocked = ${updated_total_time_clocked}
+				WHERE id = '${id}'
+				RETURNING *;
+			`;
+		}
+		else if (status === "resumed"){
+			query = `
+				UPDATE timers 
+				SET status = '${status}', last_updated_at = now()
+				WHERE id = '${id}'
+				RETURNING *;
+			`;
+		}
+	
+		const { rows } = await pgPoolClient.query(query);
+		const updated_rows = calculateTimeRemaining(rows);
+		return updated_rows[0]
+	}
 }
 
 module.exports.updateTimer = updateTimer;
